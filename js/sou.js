4cc39653e0ab64c837b77e7ebd4507fcb90636a1
miiiku <91419315+afawfa@users.noreply.github.com>
2025-07-16 16:16:14 +0800

    Update sou.js

--- js/sou.js
+++ js/sou.js
@@ -33,13 +33,6 @@ var se_list_preinstall = {
         name: "q",
         img: "./icon/bing.ico",
     },
-    '4': {
-        id: 4,
-        title: "yandex",
-        url: "https://yandex.com/search",
-        name: "text",
-        img: "https://pic.miiiku.com/file/imiiiku/1752643105069_FaxTmrhW.png",
-    },
 };
 
 // 主页快捷方式【预设】
